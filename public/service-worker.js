7df339e5fe14fb2066d23a4db12bdf7ba0bc6e89
Irene Asay <irene.hsu.asay@gmail.com>
2021-01-11 21:42:29 +0800

    fix bugs in the offline mode

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
+    // "/index.html",
     "db.js",
     "favicon.ico",
     "index.js",
     "manifest.webmanifest",
-    "service-worker.js",
+    // "service-worker.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "styles.css"
